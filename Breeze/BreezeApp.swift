134e92dd46069dc5209ca220bb7fa7426c2e5e45
YRCV <yrc@njit.edu>
2026-03-15 21:26:42 -0400

    Initial Commit

---
+++ Breeze/BreezeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreezeApp.swift
+//  Breeze
+//
+//  Created by Yahil Corcino on 3/15/26.
+//
+
+import SwiftUI
+
+@main
+struct BreezeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
